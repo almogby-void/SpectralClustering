54b222159b8b241f12a59f0bbce60612d12147fa
Almog Ben‐Yochanan <benyochanan@mail.tau.ac.il>
2022-03-16 15:36:41 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-from setuptools import setup, Extension
+from setuptools import Extension, setup
 
-setup(
-    name='myproject',
-    version='1.0.0',
-    description="Project",
-    ext_modules=[Extension('myproject', sources=['spkmeansmodule.c'])])
+module = Extension('myproject',
+                  sources=[
+                    'spkmeans',
+                    'spkmeansmodule.c'
+                  ])
+setup(name='myproject',
+     version='1.0.0',
+     description='Python wrapper for C project',
+     ext_modules=[module])
